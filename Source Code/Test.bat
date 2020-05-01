e3f240858a1e0381f5dd26645903618e5dcdc79f
Ankitamehra93 <48797537+Ankitamehra93@users.noreply.github.com>
2020-05-01 23:15:33 +0530

    First Commit

---
+++ Source Code/Test.bat
@@ -0,0 +1,13 @@
+@Echo off
+Cls
+Cd Files
+Title Box Function 3.0 - DEMO - www.thebateam.org
+
+Echo. If You can see the Diffrence Between Diffrent Boxes...
+
+Call Box 2 2 10 20 - - 2a
+Call Box 24 2 10 20 - - a2
+Call Box 2 14 10 20 - - CE
+Call Box 24 14 10 20 - - EC
+
+pause >nul
